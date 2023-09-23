a2bd28d8116678436d0aa24e96f7dd965aeec1ae
cxp03 <cxp030509@gmail.com>
2023-09-23 21:06:41 +0800

    1

--- example_instructions.py
+++ example_instructions.py
@@ -2,11 +2,10 @@
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 from typing import Optional
-
 import fire
-
 from llama import Llama
-
+import os
+from bs4 import BeautifulSoup
 
 def main(
     ckpt_dir: str,
@@ -14,7 +13,7 @@ def main(
     temperature: float = 0.2,
     top_p: float = 0.95,
     max_seq_len: int = 512,
-    max_batch_size: int = 8,
+    max_batch_size: int = 1,
     max_gen_len: Optional[int] = None,
 ):
     generator = Llama.build(
@@ -24,45 +23,55 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    instructions = [
-        [
-            {
-                "role": "user",
-                "content": "In Bash, how do I list all text files in the current directory (excluding subdirectories) that have been modified in the last month?",
-            }
-        ],
-        [
-            {
-                "role": "user",
-                "content": "What is the difference between inorder and preorder traversal? Give an example in Python.",
-            }
-        ],
-        [
-            {
-                "role": "system",
-                "content": "Provide answers in JavaScript",
-            },
-            {
-                "role": "user",
-                "content": "Write a function that computes the set of sums of all contiguous sublists of a given list.",
-            }
-        ],
-    ]
-    results = generator.chat_completion(
-        instructions,  # type: ignore
-        max_gen_len=max_gen_len,
-        temperature=temperature,
-        top_p=top_p,
-    )
+    folder_path = 'problem_descriptions'
+    all_instructions = []
+    index = []
 
-    for instruction, result in zip(instructions, results):
-        for msg in instruction:
-            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-        print(
-            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.html'):
+            index.append(filename.removesuffix('.html'))
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, 'r', encoding='utf-8') as f:
+                html_content = f.read()
+            soup = BeautifulSoup(html_content, 'html.parser')
+            single_file_instructions = []
+            single_file_instructions.append({"role": "system", "content": "Provide answers in Python"})
+            question_text = ''
+            # for p in soup.find_all('p'):
+            #     question_text += p.text
+            # single_file_instructions.append({"role": "user", "content": question_text})
+            # all_instructions.append(single_file_instructions)
+            entire_text = soup.get_text()
+            position = entire_text.find("Output")
+
+            if position != -1:
+                question_text = entire_text[:position]
+
+            single_file_instructions.append({"role": "user", "content": question_text})
+            all_instructions.append(single_file_instructions)
+
+
+
+    output_folder = "output_folder"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    i = 0
+    for idx, instruction in enumerate(all_instructions):
+        results = generator.chat_completion(
+            [instruction],
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
         )
-        print("\n==================================\n")
 
+        result = results[0]
+        output_filename = f"{output_folder}/{index[i]}.txt"
+        i += 1
+        with open(output_filename, 'w', encoding='utf-8') as f:
+            for msg in instruction:
+                f.write(f"{msg['role'].capitalize()}: {msg['content']}\n")
+            f.write(f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}\n")
 
 if __name__ == "__main__":
     fire.Fire(main)
